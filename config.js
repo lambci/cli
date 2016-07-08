ffd91dbf961bfa37e4ab99aac1aedb164e5e5320
Michael Hart <michael.hart.au@gmail.com>
2016-07-08 15:17:42 -0400

    Add reference to GitHub issue

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ exports.VERSION = require('./package.json').version
 exports.REGION = require('awscred').loadRegionSync() || 'us-east-1'
 exports.STACK = 'lambci'
 
-// The JS AWS SDK doesn't currently choose the region correctly
+// The JS AWS SDK doesn't currently choose the region from config files:
+// https://github.com/aws/aws-sdk-js/issues/1039
 require('aws-sdk').config.update({region: exports.REGION})
